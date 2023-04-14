8fdf0e7a270abd84c89f264c776a919b53c2f5c5
Xavier <xavierdias24@gmail.com>
2023-04-14 12:19:44 +0200

    Fix typing

--- board/board_wrapper.py
+++ board/board_wrapper.py
@@ -1,9 +1,9 @@
-from typing import Dict, List, Optional
-from chess import Board, IllegalMoveError, Move, PieceType, PAWN, KNIGHT, BISHOP, ROOK, QUEEN, KING
+from typing import Dict, List, Optional, Tuple
+from chess import Board, IllegalMoveError, LegalMoveGenerator, Move, PieceType, PAWN, KNIGHT, BISHOP, ROOK, QUEEN, KING
 
 
 
-PIECE_SCORES: Dict[str, int] = {
+PIECE_SCORES: Dict[int, int] = {
     PAWN: 1,
     KNIGHT: 3,
     BISHOP: 3,
@@ -31,11 +31,25 @@ class BoardWrapper:
         return self.is_draw() or self.is_checkmate()
     
     def get_possible_moves(self) -> List[Move]:
-        return self.board.legal_moves
+        return list(self.board.legal_moves)
 
     def get_possible_sorted_moves(self) -> List[Move]:
         moves: List[Move] = self.get_possible_moves()
         return self._sort_moves(moves)
+
+    def _sort_moves(self, moves: List[Move]) -> List[Move]:
+        pieces: List[Optional[PieceType]] = [
+            self.board.piece_type_at(move.to_square) 
+            for move in moves
+        ]
+
+        moves_sorted: List[Tuple[Move, Optional[int]]] = sorted(
+            zip(moves, pieces),
+            key=lambda x: -1 * float('inf') if x[1] is None else x[1],
+            reverse=True
+        )
+
+        return list(map(lambda x: x[0], moves_sorted))
 
     def make_move(self, move: Move) -> None:
         if not self.board.is_legal(move):
@@ -48,20 +62,6 @@ class BoardWrapper:
     
     def get_last_move(self) -> Move:
         return self.board.peek()
-
-    def _sort_moves(self, moves: List[Move]) -> List[Move]:
-        pieces: List[Optional[PieceType]] = [
-            self.board.piece_type_at(move.to_square) 
-            for move in moves
-        ]
-
-        moves = sorted(
-            zip(moves, pieces),
-            key=lambda x: -1 * float('inf') if x[1] is None else x[1],
-            reverse=True
-        )
-
-        return map(lambda x: x[0], moves)
     
     def evaluate_state(self, player: bool) -> int:
         total_score: int = 0
